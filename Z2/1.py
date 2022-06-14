11f3e2917a54c7847cb90a33e8f0b6d3fd45de7e
Icolinio <94836511+Icolinio@users.noreply.github.com>
2022-06-14 13:48:58 +0200

    Add files via upload

--- Z2/1.py
+++ Z2/1.py
@@ -1,15 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-wyk1 = [39,40,16,45,37]
-wyk2 = [-37,-36,-16,-33,-37]
+wyk1 = [39, 40, 16, 45, 37]
+wyk2 = [-37, -36, -16, -33, -37]
 Y = np.arange(5)
 plt.subplot(1, 2, 1)
-plt.barh(Y,wyk1, color=['lightblue','pink','orange','gray','purple'], height=0.75, label="A",)
+plt.barh(Y, wyk1, color=['lightblue', 'pink', 'orange', 'gray', 'purple'], height=0.75, label="A", )
 plt.title("Wykres lewy")
-plt.yticks(Y,['A','B','C','D','E'])
+plt.yticks(Y, ['A', 'B', 'C', 'D', 'E'])
 plt.subplot(1, 2, 2)
-plt.barh(Y, wyk2, color=['pink','blue','lightblue','brown','yellow'], height=0.75, label="B")
+plt.barh(Y, wyk2, color=['pink', 'blue', 'lightblue', 'brown', 'yellow'], height=0.75, label="B")
 plt.title("Wykres lewy")
-plt.yticks(Y,['A','B','C','D','E'])
+plt.yticks(Y, ['A', 'B', 'C', 'D', 'E'])
 plt.show()
